7633bc4af9059a3d1c0126d70c906557bd14b1b4
wcrandall <aweserific@gmail.com>
2020-11-22 21:10:27 -0600

    took out using statements

--- Timer/MainWindow.xaml.cs
+++ Timer/MainWindow.xaml.cs
@@ -1,26 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Timer
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window, INotifyPropertyChanged
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
 	{
 		private DispatcherTimer _dispatcherTimer = new DispatcherTimer();
 		private bool _isRunning = false;
@@ -201,14 +191,17 @@ namespace Timer
         private void SwitchedToStopwatch(object sender, MouseButtonEventArgs e)
         {
 
-			_isStopwatch = true; 
+			_isStopwatch = true;
+
 			Stop();
-        }
+		}
 
         private void SwitchedToCountdown(object sender, MouseButtonEventArgs e)
         {
+			
 			_isStopwatch = false;
-			Stop(); 
-        }
-    }
+			Stop();
+
+		}
+	}
 }
